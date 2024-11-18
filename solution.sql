26a6d6f8e191db5b3e36de65d19a26117e8623e2
rorymoxom <159725779+rorymoxom@users.noreply.github.com>
2024-11-18 11:22:10 +0000

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -6,8 +6,8 @@ select *
 from crime_scene_report
 where type = 'murder' and city = "SQL City" and date = 20180115
 
-//Security footage shows that there were 2 witnesses. The first witness lives at the last house on "Northwestern Dr". 
-The second witness, named Annabel, lives somewhere on "Franklin Ave".
+--Security footage shows that there were 2 witnesses. The first witness lives at the last house on "Northwestern Dr". 
+-- The second witness, named Annabel, lives somewhere on "Franklin Ave".
 
 select 
 name,
